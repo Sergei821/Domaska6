de8ca2b2991dcceac561ab80d14d3e4a0f7a68df
Sergei821 <ps16111982@gmail.com>
2023-04-23 21:13:04 +0300

    zad1

---
+++ Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+
+int InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputNumberWithMessage($"Введите {i + 1} число: ");
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count = count + 1;
+
+    }
+    return count;
+}
+
+int length = InputNumberWithMessage("Введите количество чисел: ");
+int[] nunbers = new int[length];
+
+
+FillArray(nunbers);
+PrintArray(nunbers);
+
+int result = CountPositiveNumbers(nunbers);
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество положительных чисел: {result}");
+
+
+
